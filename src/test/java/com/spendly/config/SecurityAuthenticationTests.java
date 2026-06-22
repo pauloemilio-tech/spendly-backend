389c50de6e4c36027c747800c084484e8b426b6f
Paulo Toledo <paulojrtoledo@gmail.com>
2026-06-22 19:39:32 -0300

    test: isolate Spring context tests

--- src/test/java/com/spendly/config/SecurityAuthenticationTests.java
+++ src/test/java/com/spendly/config/SecurityAuthenticationTests.java
@@ -1,9 +1,20 @@
 package com.spendly.config;
 
+import com.spendly.domain.repository.CustomerRepository;
+import com.spendly.domain.repository.TransactionRepository;
+import com.spendly.domain.repository.WalletRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@MockitoBean(types = {
+        CustomerRepository.class,
+        TransactionRepository.class,
+        WalletRepository.class
+})
 class SecurityAuthenticationTests {
 
     @Test
